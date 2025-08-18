28ed43fc311a1341c15e4fe25110f962a598686e
RODRIGO TAVARES SILVA <rodrigolk2012@gmail.com>
2025-08-18 23:11:10 +0000

    Cartas_super_trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     printf("Desafio Super Trunfo!\n");
-    
+        
     char codigo1[] = "GO01";
     char nomeCidade1[] = "Goiânia";
     int populacao1 = 1437366;
@@ -17,21 +17,20 @@ int main() {
     float pib2 = 63.0;
     int pontosTuristicos2 = 60;
 
-    printf("\n================== CARTA 1 ==================\n");
-    printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomeCidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.1f km²\n", area1);
-    printf("PIB: %.1f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+printf("\n================== CARTA 1 ==================\n");
+printf("Código: %s\n", codigo1);
+printf("Nome da Cidade: %s\n", nomeCidade1);
+printf("População: %d\n", populacao1);
+printf("Área: %.1f km²\n", area1);
+printf("PIB: %.1f bilhões de reais\n", pib1);
+printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
-    printf("\n================== CARTA 2 ==================\n");
-    printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nomeCidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.1f km²\n", area2);
-    printf("PIB: %.1f bilhões de reais\n", pib2);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
-
-    return 0;
+printf("\n================== CARTA 2 ==================\n");
+printf("Código: %s\n", codigo2);
+printf("Nome da Cidade: %s\n", nomeCidade2);
+printf("População: %d\n", populacao2);
+printf("Área: %.1f km²\n", area2);
+printf("PIB: %.1f bilhões de reais\n", pib2);
+printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+return 0;
 }
